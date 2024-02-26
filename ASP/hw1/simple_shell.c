da70183374a89cb4c56f0102046aa654c5416dfd
headheadhead <b08902062@csie.ntu.edu.tw>
2024-02-26 23:19:38 +0800

    ASP HW1: Parsing commands separated by pipe
    Also fix some minor bugs.

--- ASP/hw1/simple_shell.c
+++ ASP/hw1/simple_shell.c
@@ -22,34 +22,56 @@
 } while (0)
 
 static bool is_end;
+static char *cmd_history[10];
 
-int parse_args(char **raw_cmd_ptr, char ***args_ptr)
+int parse_args(char *raw_input, char ***args_ptr)
 {
 	char *token, *arg;
 	char *delimiter = "\n ";
 	int args_len = 0;
 	int args_size = 16;
 
-	if (strlen(*raw_cmd_ptr) > _POSIX_ARG_MAX)
-		return -EINVAL;
-
 	*args_ptr = (char **)malloc(sizeof(char *)*args_size);
-	token = strtok(*raw_cmd_ptr, delimiter);
+	token = strtok(raw_input, delimiter);
 
 	while (token) {
-		if (args_len+1 > args_size) {
+		if (args_len+1 >= args_size) {
 			args_size *= 2;
-			*args_ptr = (char **)malloc(sizeof(char *)*args_size);
+			if (args_size >= _POSIX_ARG_MAX)
+				return -EINVAL;
+			*args_ptr = (char **)realloc(*args_ptr, sizeof(char *) * args_size);
 		}
-		arg = malloc(strlen(token));
+		arg = malloc(strlen(token) + 1);
 		if (!arg)
-			return -1;
+			return -errno;
 
 		strcpy(arg, token);
+		fprintf(stderr, "args[%d]: %s\n", args_len, arg);
 		(*args_ptr)[args_len++] = arg;
 		token = strtok(NULL, delimiter);
 	}
 	(*args_ptr)[args_len] = NULL;
+
+	return 0;
+}
+
+int parse_cmds(char *raw_input, char ****args_arr_ptr)
+{
+	const char *pipe_deli = "|";
+	char *cmd_token;
+	int args_arr_len = 0, args_arr_size = 16;
+
+	*args_arr_ptr = (char ***)malloc(sizeof(char **) * args_arr_size);
+	while ((cmd_token = strsep(&raw_input, pipe_deli))) {
+		fprintf(stderr, "args_arr[%d]: %s\n", args_arr_len, cmd_token);
+		if (args_arr_len+1 >= args_arr_size) {
+			args_arr_size *= 2;
+			*args_arr_ptr = (char ***)realloc(*args_arr_ptr,
+					sizeof(char **) * args_arr_size);
+		}
+		parse_args(cmd_token, (*args_arr_ptr) + args_arr_len++);
+	}
+	(*args_arr_ptr)[args_arr_len] = NULL;
 
 	return 0;
 }
@@ -65,16 +87,30 @@ void free_args(char ***args_ptr)
 		free(*arg_ptr);
 		*arg_ptr = NULL;
 	}
-	free(args_ptr);
+	free(*args_ptr);
 	*args_ptr = NULL;
 }
 
-void free_resources(char ***args_ptr, char **raw_cmd_ptr) {
-	if (*raw_cmd_ptr) {
-		free(*raw_cmd_ptr);
-		*raw_cmd_ptr = NULL;
+void free_args_arr(char ****args_arr_ptr)
+{
+	char ***args_ptr;
+
+	if (!*args_arr_ptr)
+		return;
+
+	for (args_ptr = *args_arr_ptr; *args_ptr; args_ptr++)
+		free_args(args_ptr);
+	free(*args_arr_ptr);
+	*args_arr_ptr = NULL;
+}
+
+void free_resources(char ****args_arr_ptr, char **raw_input_ptr)
+{
+	if (*raw_input_ptr) {
+		free(*raw_input_ptr);
+		*raw_input_ptr = NULL;
 	}
-	free_args(args_ptr);
+	free_args_arr(args_arr_ptr);
 }
 
 void signal_handler(int signum)
@@ -105,30 +141,30 @@ int unblock_sigint(void)
 	return 0;
 }
 
-int read_input(char **raw_cmd_ptr)
+int read_input(char **raw_input_ptr)
 {
 	char ch;
-	int raw_cmd_len = 0;
-	int raw_cmd_size = 1024;
+	int raw_input_len = 0;
+	int raw_input_size = 1024;
 
-	*raw_cmd_ptr = malloc(raw_cmd_size);
+	*raw_input_ptr = malloc(raw_input_size);
 	RET_IF_ERR(unblock_sigint());
 	while ((ch = getchar()) != EOF && !is_end) {
 		RET_IF_ERR(ch);
 		RET_IF_ERR(block_sigint());
 
-		if (raw_cmd_len+1 > raw_cmd_size) {
-			raw_cmd_size *= 2;
-			*raw_cmd_ptr = realloc(*raw_cmd_ptr, raw_cmd_size);
+		if (raw_input_len+1 >= raw_input_size) {
+			raw_input_size *= 2;
+			*raw_input_ptr = realloc(*raw_input_ptr, raw_input_size);
 		}
-		if (!*raw_cmd_ptr)
+		if (!*raw_input_ptr)
 			return -1;
-		(*raw_cmd_ptr)[raw_cmd_len++] = ch;
+		(*raw_input_ptr)[raw_input_len++] = ch;
 		RET_IF_ERR(unblock_sigint());
 		if (ch == '\n')
 			break;
 	}
-	(*raw_cmd_ptr)[raw_cmd_len] = '\0';
+	(*raw_input_ptr)[raw_input_len] = '\0';
 
 	if (is_end)
 		return -EINTR;
@@ -140,7 +176,8 @@ int read_input(char **raw_cmd_ptr)
 int main(int argc, char *argv[])
 {
 	int ret, pid;
-	char *raw_cmd = NULL;
+	char *raw_input = NULL;
+	char ***args_arr = NULL;
 	char **args = NULL;
 	struct sigaction sigint_action;
 
@@ -155,14 +192,16 @@ int main(int argc, char *argv[])
 		GOTO_IF_ERR(printf("$"), ret, err);
 		GOTO_IF_ERR(block_sigint(), ret, err);
 
-		GOTO_IF_ERR(read_input(&raw_cmd), ret, err);
-		if (strncmp(raw_cmd, "\n", 1) == 0) {
-			free(raw_cmd);
-			raw_cmd = NULL;
+		GOTO_IF_ERR(read_input(&raw_input), ret, err);
+		if (strncmp(raw_input, "\n", 1) == 0) {
+			free(raw_input);
+			raw_input = NULL;
 			continue;
 		}
-		GOTO_IF_ERR(parse_args(&raw_cmd, &args), ret, err);
+		GOTO_IF_ERR(parse_cmds(raw_input, &args_arr), ret, err);
 
+		free_resources(&args_arr, &raw_input);
+		continue;
 		if (strncmp(args[0], "cd", 2) == 0)
 			GOTO_IF_ERR(chdir(args[1]), ret, err);
 		else if (strncmp(args[0], "exit", 4) == 0)
@@ -180,13 +219,13 @@ int main(int argc, char *argv[])
 		if (pid != -1)
 			GOTO_IF_ERR(wait(NULL), ret, err);
 
-		free_resources(&args, &raw_cmd);
+		free_resources(&args_arr, &raw_input);
 		GOTO_IF_ERR(printf("\n"), ret, err);
 	}
 
 	exit(0);
 err:
-	free_resources(&args, &raw_cmd);
+	free_resources(&args_arr, &raw_input);
 	fprintf(stderr, "error: %s\n", strerror(errno));
 	exit(errno);
 }
